96dddf2a7554c3cdf8f9762fa4ba86643a7dcb0b
disenotov <mitri4cool@mail.ru>
2023-03-28 18:00:08 +0300

    feat: PT-1491 add renounce adminship migration

--- tasks/deploy/RenounceAdminship.ts
+++ tasks/deploy/RenounceAdminship.ts
@@ -7,35 +7,43 @@ type NetworkSettings = {
   rariMineAddress: string;
 }
 
-const mainnet = {
+type Settings = {
+  mainnet: NetworkSettings;
+  goerli: NetworkSettings;
+  default: NetworkSettings;
+}
+
+
+const mainnet: NetworkSettings = {
 	timelock: "0x7e9c956e3EFA81Ace71905Ff0dAEf1A71f42CBC5",
   adminProxyAddress: "0xDc8BaA86f136F8B0851F090a4DfFDc7b5F46688D",
   lockingAddress: "0x096Bd9a7a2e703670088C05035e23c7a9F428496",
   rariMineAddress: "0xc633F65A1BEBD433DF12D9F3ac7aCF31b26Ca1E6"
 }
 
-const goerli = {
+const goerli: NetworkSettings = {
 	timelock: "0x1C795d4AEf47BBbf0698471A3b04D51b2BFac893",
   adminProxyAddress: "0x919AEd466F30A821670b12aaab3A4102d8536486",
   lockingAddress: "0x39C9D13e1b17Bf1975aFe892e18B1D5A1482b52D",
   rariMineAddress: "0xD335eEc939Ff164AF82e2ab97B6c16380308eCEB"
 }
-const def = {
+
+const def: NetworkSettings = {
 	timelock: "0x0000000000000000000000000000000000000000",
   adminProxyAddress: "0x0000000000000000000000000000000000000000",
   lockingAddress: "0x0000000000000000000000000000000000000000",
   rariMineAddress: "0x0000000000000000000000000000000000000000"
 }
 
-let settings = {
+let settings: Settings = {
 	"default": def,
 	"mainnet": mainnet,
 	"goerli": goerli
 };
 
 function getSettings(network: string) : NetworkSettings {
-	if (settings[network] !== undefined) {
-		return settings[network];
+	if (settings[network as keyof Settings] !== undefined) {
+		return settings[network as keyof Settings];
 	} else {
 		return settings["default"];
 	}
@@ -50,9 +58,6 @@ async function main() {
   console.log("settings:", set)
 
   console.log("Deploying contracts with the account:", deployer.address);
-  
-  console.log(await upgrades.admin.getInstance())
- 
   
   //changing owner of locking to timelock
   const Locking = await ethers.getContractFactory("Locking");
@@ -73,13 +78,13 @@ async function main() {
 
 }
 
-async function changeOwner(contract, newOwner, contractName) {
+async function changeOwner(contract: any, newOwner: string, contractName: string) {
   const oldOwner = await contract.owner();
   await contract.transferOwnership(newOwner)
   console.log(`for contract ${contractName} at ${contract.address} changed owner from ${oldOwner} to ${await contract.owner()}`)
 }
 
-async function changeAdminProxy(admin, contract, newAdmin, contractName) {
+async function changeAdminProxy(admin: any, contract : any, newAdmin: string, contractName: string) {
   const oldAdmin = await admin.getProxyAdmin(contract.address)
   await admin.changeProxyAdmin(contract.address, newAdmin)
   const ProxyAdmin = await ethers.getContractFactory("ProxyAdmin");
