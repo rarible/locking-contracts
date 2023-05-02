415e113d201d831e7f07cad9f6cc5e0c8e4e5370
Vadim Fadeev <xboxfadeev@gmail.com>
2023-05-02 17:25:30 +0100

    Update comment.js

--- .github/scripts/comment.js
+++ .github/scripts/comment.js
@@ -1,5 +1,5 @@
 module.exports = async ({ github, context, header, body }) => {
-    console.log("script start");
+    console.log("script start escape");
     const comment = [header, body].join("\n");
     console.log("script comment");
     const { data: comments } = await github.rest.issues.listComments({
@@ -23,7 +23,7 @@ module.exports = async ({ github, context, header, body }) => {
     await github.rest.issues[commentFn]({
       owner: context.repo.owner,
       repo: context.repo.repo,
-      body: comment,
+      body: escape(comment),
       ...(botComment
         ? { comment_id: botComment.id }
         : { issue_number: context.payload.number }),
